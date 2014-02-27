f36dd711f8930c2d6e6cca7eb9a1a20fb15fda78
nskitch <nskith@griddynamics.com>
2014-02-26 17:11:31 -0800

    fixed bug in script for curl

--- setIPsforEnv.sh
+++ setIPsforEnv.sh
@@ -5,29 +5,23 @@
 export envDetailsUrl='http://mdc2vr4073:8888/NewEnvironmentDetails/EnvironmentDetails.html'
 
 export envName=$1
-if [[ -z "$envName" ]]
-then 
+if [[ -z "$envName" ]]; then
 	export envName=mcominternal5017
 	echo "No environment specified so defaulting to hard-coded value $envName" 
 fi
 
 echo "scraping page to get IPs of Nodes for Macys: $envDetailsUrl"
-export curlOutput=''
-export curlOutput=`curl -s -d "envName=$envName" "$envDetailsUrl"`
+STATUS=$(curl -s -d "envName=$envName" "$envDetailsUrl" -w '%{http_code}' -o /tmp/$envName)
 
-if [[ -z "$curlOutput" ]]
-then
+if [ $STATUS -ne 200 ]; then
 	echo "page unreachable; exiting. $envDetailsUrl"
 	exit
 fi
 
-echo $curlOutput > /tmp/$envName
-
-
 export legacyIP=`grep 'Legacy' /tmp/$envName | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}'`  
 export navappIP=`grep 'NavApp' /tmp/$envName | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}'` 
 export sdpIP=`grep 'SDP' /tmp/$envName | grep 'SDPClient' | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}'`  
-export db2IP=`grep 'DB2' /tmp/$envName | |grep 'SITE-ASYNC-DB2'|grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}'` 
+export db2IP=`grep 'DB2' /tmp/$envName | grep 'SITE-ASYNC-DB2'| grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}'` 
 
 echo "The following vars have been set:"
 echo "legacyIP=$legacyIP"
