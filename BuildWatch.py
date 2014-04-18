fbeb3027e3a1d4b485d918fc27a3677edafb2e47
nskitch <nskith@griddynamics.com>
2014-04-18 14:27:59 -0700

    restored buildstatus after git destroyed it

--- BuildWatch.py
+++ BuildWatch.py
@@ -2,6 +2,12 @@ __author__ = 'Nick'
 
 import time
 import subprocess
+import re
+import sys
+
+requestId='35236'
+
+jsession1=''
 
 # attempt to read in credentials from file that will not be checked in to vcs
 try:
@@ -10,8 +16,19 @@ except ImportError:
    pass
 
 
-#curl -c -s cookies.txt -d "userLogin.username=yc14ns1&userLogin.password=nisum2019" http://mdc2vr4073:9090/MacysOrchestration/logout.html
-
+def setToken():
+    print 'logging in .. '
+    global jsession1    # Needed to modify global copy of globvar
+    #cmd = 'curl -c -s cookies.txt -d "userLogin.username=' + USERNAME + '&userLogin.password=' + PASS1 + ' http://mdc2vr4073:9090/MacysOrchestration/logout.html'
+    cmd = 'curl -sL -c cookies.txt -d "userLogin.username=' + USERNAME + '&userLogin.password=' + PASS1 + '" "http://mdc2vr4073:9090/MacysOrchestration/logout.html"'
+    print cmd
+    subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).stdout.read().rstrip()
+    cookie=open('cookies.txt', 'r')
+    cookieText = '\n'.join(cookie.readlines())
+    validHash = re.finditer(r'(?=(\b[A-Fa-f0-9]{32}\b))', cookieText)
+    result = [match.group(1) for match in validHash]
+    jsession1=result[0]
+    print 'session id set: ' + jsession1
 
 
 
@@ -20,7 +37,25 @@ def message(message):
     print cmd2
     subprocess.Popen(cmd2, shell=True, stdout=subprocess.PIPE).stdout.read().rstrip()
 
-
+setToken()
 while True:
+    cmd = 'curl -s \'http://mdc2vr4073:9090/MacysOrchestration/final-status.html?requestId='+requestId+'\' -H \'Accept-Encoding: gzip,deflate,sdch\' -H \'Accept-Language: en-US,en;q=0.8\' -H \'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.116 Safari/537.36\' -H \'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\' -H \'Cache-Control: max-age=0\' -H \'Cookie: JSESSIONID='+jsession1+'; redirectUrl="/MacysOrchestration/env-live-status.html?requestId='+requestId+'\' -H \'Connection: keep-alive\' --compressed'
 
-    time.sleep(30)
+    response = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).stdout.read().rstrip()
+
+    if len(response)==0:
+        print 'session expired; no response'
+        setToken()
+
+    if (response.find('RUNNING')==-1) or (response.find('NOTSTARTED')==-1):
+        if 'COMPLETED' in response:
+            message('RAPAD ' + requestId +' Completed status detected')
+        if 'FAILED' in response:
+            message('RAPAD ' + requestId + ' Failed status detected')
+
+        #or (response.find('NOTSTARTED')>1))
+
+    sys.stdout.write('.')
+
+
+    time.sleep(50)
--- bitcoinAlert.py
+++ bitcoinAlert.py
@@ -1,1 +1,82 @@
 __author__ = 'Nick'
+import subprocess
+import decimal
+import time
+import library
+
+import os
+def tail(f, n, offset=0):
+    """Reads a n lines from f with an offset of offset lines."""
+    avg_line_length = 74
+    to_read = n + offset
+    while 1:
+        try:
+            f.seek(-(avg_line_length * to_read), 2)
+        except IOError:
+            # woops.  apparently file is smaller than what we want
+            # to step back, go to the beginning instead
+            f.seek(0)
+        pos = f.tell()
+        lines = f.read().splitlines()
+        if len(lines) >= to_read or pos == 0:
+            return lines[-to_read:offset and -offset or None]
+        avg_line_length *= 1.3
+
+def message(message):
+    cmd2 = "terminal-notifier -message '" + str(message) + "'"
+    print cmd2
+    subprocess.Popen(cmd2, shell=True, stdout=subprocess.PIPE).stdout.read().rstrip()
+
+
+while True:
+
+    if not library.isPingable('bcchanger.com'):
+        break
+
+    lastBitcoinPricef=open("/tmp/lastBitcoinPrice.txt", "r")
+
+    oldPrice=tail(lastBitcoinPricef,1)
+    lastBitcoinPricef.close()
+    print oldPrice
+
+    bitcoinPriceUrl='http://bcchanger.com/bitcoin_price_feed.php?feed_type=text&currency=USD'
+
+    cmd2 = "curl -s "+bitcoinPriceUrl
+
+    bitcoinPrice = subprocess.Popen(cmd2, shell=True, stdout=subprocess.PIPE).stdout.read().rstrip()
+
+    bitcoinPriceSplit = bitcoinPrice.split('.')
+    bitcoinPrice=bitcoinPriceSplit[0]
+    print bitcoinPrice
+
+    prices = [int(bitcoinPrice),int(oldPrice[0])]
+
+    for a, b in zip(prices[::1], prices[1::1]):
+        percentDiff = 100 * (b - a) / a
+        #print str(percentDiff) + "% difference"
+
+
+    if (percentDiff < -10) or (percentDiff > 10):
+        messageText='bitcoin price is ' + str(percentDiff) + "% difference: $" + bitcoinPrice
+        message(messageText)
+
+    num=400
+    if int(bitcoinPrice) < num:
+        message("bitcoin price under " + num + " $" + bitcoinPrice)
+
+    num=500
+    if int(bitcoinPrice) > num:
+        message("bitcoin price under " + num + " $" + bitcoinPrice)
+
+
+    lastBitcoinPricef=open("/tmp/lastBitcoinPrice.txt", "a")
+    lastBitcoinPricef.writelines(bitcoinPrice+"\n")
+
+    lastBitcoinPricef.writelines('10\n')
+    lastBitcoinPricef.close()
+
+    # if it's six AM, exit, so the next cron job can start at 7
+    if time.strftime("%H") == '06':
+        exit()
+
+    time.sleep(600)
