57a52c85f957d2b5703e548bdf06e2da035dd2cc
nickskitch <nickskitch@gmail.com>
2015-11-06 13:29:28 -0800

    Update triggerBrightBuild.py

--- triggerBrightBuild.py
+++ triggerBrightBuild.py
@@ -17,7 +17,7 @@ base_url='http://11.120.101.188:8080/jenkins/'
 j = jenkins.Jenkins(base_url, username, password)
 
 
-j.build_job('MCOM_Dev_WishList', {'daServiceAccountUser': 'da-MCOM-WDSDevOps', 'daServiceAccountPasswd': 'ph8pumaK95he', 'targetEnv': 'mcominternal5024', 'project': 'Wishlist', 'tags': '@'+ BRANCH, 'gitBranch': 'master'})
+j.build_job('MCOM_Dev_WishList', {'daServiceAccountUser': 'da-MCOM-WDSDevOps', 'daServiceAccountPasswd': '', 'targetEnv': 'mcominternal5024', 'project': 'Wishlist', 'tags': '@'+ BRANCH, 'gitBranch': 'master'})
 
 
 f = open('/Users/Nick/scripts/macys/triggerBrightBuildHistory.txt','a')
